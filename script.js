8737f357ece00ada5fe1ec22f24f96550a4b9b71
Yasmeen hani <yasmeen.hani@outlook.com>
2025-11-23 13:30:20 +0300

    login

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let username;
 let password;
 let email;
+let accountnum = -1;
+
 
 document.getElementById("login_button")?.addEventListener("click", function() {
     if (username === undefined || password === undefined) {
@@ -13,6 +15,34 @@ document.getElementById("login_button")?.addEventListener("click", function() {
     password = document.getElementById("password").value;
 
     
+});
+
+
+// For storage of the data we make new items by doing a localStorage.setItem(name, value)
+// then to retrieve it we use getItem(name)
+
+
+
+document.getElementById("register_button").addEventListener("click", function(){
+    
+    let checkpass1, checkpass2;
+
+    checkpass1 = document.getElementById("new_password").value;
+    checkpass2 = document.getElementById("confirm_new_password").value;
+
+    if (checkpass1 === checkpass2){
+        alert("Your account is registered")
+            accountnum += 1;
+            let key = "accountname" + accountnum;
+            localStorage.setItem(key, )
+            username = document.getElementById("new_username").value;
+            password = document.getElementById("new_password").value;
+            email = document.getElementById("new_email").value;
+            localStorage
+    }else{
+        alert("passwords dont match")
+    }
+
 });
 
 document.addEventListener('DOMContentLoaded', () => {
